86d4d7bd20ae63550d07ed06704fccff4610d271
yijiliu <musicx@outlook.com>
2014-04-18 18:12:35 +0800

    extend the merge function to wildchar filtering and remove the type reference function.

--- Package/PythonGainChart/merge.py
+++ Package/PythonGainChart/merge.py
@@ -1,3 +1,5 @@
+import fnmatch
+import json
 from optparse import OptionParser
 from collections import OrderedDict
 import os
@@ -37,33 +39,43 @@ class IterRows:
             self.file_handler.close()
             raise StopIteration
         parts = line.strip().split(self.delimiter)
-        variable_values = [parts[x].strip() for x in xrange(len(parts)) if x in self.valid_inds]
+        variable_values = [parts[i].strip() for i in xrange(len(parts)) if i in self.valid_inds]
         return tuple(variable_values)
 
 
-def parse_type(file_path) :
+def parse_type(type_string) :
     # this special type should contain "num, char, drop, keep" 4 types
     # the logic of keep / drop is : if keep, the drop all others, otherwise, drop listed.
-    special = {}
     ikeeps = set()
     idrops = set()
-    with open(file_path) as fn:
-        while True:
-            line = fn.readline()
-            if not line :
-                break
+    if os.path.exists(type_string) :
+        lines = open(type_string).readlines()
+        for line in lines:
             parts = [x.lower().strip() for x in line.split(',')]
-            if "num" in parts :
-                special[parts[0]] = "num"
-            elif "char" in parts :
-                special[parts[0]] = "char"
-            if "drop" in parts :
+            if "drop" in parts or 'd' in parts:
                 idrops.add(parts[0])
-            if "keep" in parts :
+            if "keep" in parts or 'k' in parts:
                 ikeeps.add(parts[0])
-        if len(ikeeps) > 0 :
-            idrops = set()
-    return special, ikeeps, idrops
+    else :
+        try :
+            variables = json.loads(type_string.lower())
+            for name in variables :
+                if 'drop' in variables[name] or 'd' in variables[name] :
+                    idrops.add(name)
+                if 'keep' in variables[name] or 'k' in variables[name] :
+                    ikeeps.add(name)
+        except ValueError :
+            pass
+    if len(ikeeps) > 0 :
+        idrops = set()
+    return ikeeps, idrops
+
+
+def findPatterns(base, patterns) :
+    selected = set()
+    for pattern in patterns :
+        selected = selected.union(fnmatch.filter(base, pattern))
+    return selected
 
 
 if __name__ == '__main__':
@@ -75,7 +87,12 @@ if __name__ == '__main__':
     parser.add_option("-d", "--dlm", dest="dlm", action="store", type="string", default=",",
                       help="delimiter char, accept xASCII format, default=,")
     parser.add_option("-t", "--type", dest="type", action="store", type="string",
-                      help="variable type / keep / drop list ordered same as data files. files separated with ';'")
+                      help="optional variable type files / json strings in format var_name[,k[eep]|d[rop]] on each line. " +
+                           "Multiple files should be separated with ';' in the same order of the input data files")
+    parser.add_option("-s", "--base", dest="keep", action="store", type="string",
+                      help="optional variable list for simple keeping, whilchar supported, variables separated with ',', files separated with ';'")
+    parser.add_option("-x", "--excl", dest="drop", action="store", type="string",
+                      help="optional variable list for simple exclusion, wildchar supported, variables separated with ',', files separated with ';'")
     parser.add_option("-o", "--output", dest="output", action="store", type="string",
                       help="output merged filename")
     (options, args) = parser.parse_args()
@@ -88,72 +105,75 @@ if __name__ == '__main__':
     if options.dlm.startswith('x') :
         options.dlm = chr(int(options.dlm[1:]))
 
-    if options.type :
-        type_files = options.type.split(";")
-    else :
-        type_files = None
     data_files = options.data.split(';')
-    if type_files is not None and len(type_files) != len(data_files) :
+    type_strings = options.type.split(";") if options.type else None
+    keep_strings = options.keep.split(';') if options.keep else None
+    drop_strings = options.drop.split(';') if options.drop else None
+    if type_strings is not None and len(type_strings) != len(data_files) :
         raise ValueError("The type list files must have 1-to-1 relationship with the dataset files")
+    if keep_strings is not None and len(keep_strings) != len(data_files) :
+        raise ValueError("The keep strings must have 1-to-1 relationship with the dataset files")
+    if drop_strings is not None and len(drop_strings) != len(data_files) :
+        raise ValueError("The drop strings must have 1-to-1 relationship with the dataset files")
 
-    if type_files is not None :
-        for type_file in type_files :
-            if not os.path.exists(type_file) :
-                raise IOError("file does not exist! {}".format(type_file))
     for data_file in data_files :
         if not os.path.exists(data_file) :
             raise IOError("file does not exist! {}".format(data_file))
 
-    type_float = ignore_exception(ValueError, None)(float)
-    known_error_value = {'', 'null', 'none', 'missing', '.', '_missing_'}
-
-    variable_set = set()
+    global_variable_set = set()
     file_headers = []
-    character_headers = []
     for dind in xrange(len(data_files)) :
         data_file = data_files[dind]
-        if type_files is not None :
-            specials, keeps, drops = parse_type(type_files[dind])
+
+        # The logic for keep/drop is :
+        # 1. go through the type file
+        #    if the type file has keep list, keep only the variables in the keep list, and drop ALL other.
+        #    otherwise, if the type file has only drop list, drop those variables
+        # 2. go through the command line input drop patterns
+        #    the keep / drop lists from type file are modified.
+        #    drop variables will be removed from earlier keep list, and add to the new drop list,
+        #    or simply add to the existing drop list.
+        # 3. go through the command line input keep patterns
+        #    continue to modify the existing keep / drop lists
+        #    keep variables will be removed from earlier drop list, and add to the keep list,
+        if type_strings is not None :
+            keeps, drops = parse_type(type_strings[dind])
         else :
-            specials = set()
             keeps = set()
             drops = set()
+
         with open(data_file) as fn :
             line = fn.readline()
             variable_names = [x.strip() for x in line.split(options.dlm)]
+            variable_names_lower = [x.lower() for x in variable_names]
+            if drop_strings is not None :
+                drop_patterns = drop_strings[dind].lower().split(',')
+                drop_variables = set(findPatterns(variable_names_lower, drop_patterns))
+                keeps = keeps.difference(drop_variables)
+                drops = drops.union(drop_variables) if len(drops) > 0 or type_strings is None else drops
+            if keep_strings is not None :
+                keep_patterns = keep_strings[dind].lower().split(',')
+                keep_variables = set(findPatterns(variable_names_lower, keep_patterns))
+                keeps = keeps.union(keep_variables) if len(keeps) > 0 or type_strings is None else keeps
+                drops = drops.difference(keep_variables)
+
             new_variable_names = OrderedDict()
             for ind in xrange(len(variable_names)) :
                 variable_name = variable_names[ind]
-                if variable_name.lower() not in drops or (len(keeps) > 0 and variable_name in keeps):
-                    if variable_name.lower() not in variable_set :
+                if variable_name.lower() not in drops or (len(keeps) > 0 and variable_name in keeps) or variable_name.lower() in keys:
+                    if variable_name.lower() in keys :
                         new_variable_names[ind] = variable_name
-                        variable_set.add(variable_name.lower())
-                if variable_name.lower() in keys :
-                    new_variable_names[ind] = variable_name
+                    elif variable_name.lower() not in global_variable_set :
+                        new_variable_names[ind] = variable_name
+                        global_variable_set.add(variable_name.lower())
+                    else :
+                        ext_variable_name = variable_name + "_dup"
+                        while ext_variable_name.lower() in global_variable_set :
+                            ext_variable_name += "_dup"
+                        new_variable_names[ind] = ext_variable_name
+                        global_variable_set.add(ext_variable_name.lower())
             file_headers.append(new_variable_names)
-            character_variables = {}
-            numeric_variables = set()
-            for ln in xrange(10000) :
-                line = fn.readline()
-                if not line :
-                    break
-                parts = [x.strip() for x in line.split(options.dlm)]
-                for pind in xrange(len(parts)) :
-                    if pind not in new_variable_names or pind in character_variables or pind in numeric_variables:
-                        continue
-                    if variable_names[pind].lower() in keys :
-                        character_variables[pind] = variable_names[pind]
-                        continue
-                    if variable_names[pind].lower() in specials :
-                        if specials[variable_names[pind].lower()] == "char" :
-                            character_variables[pind] = variable_names[pind]
-                        else :
-                            numeric_variables.add(pind)
-                        continue
-                    parsed = type_float(parts[pind])
-                    if parsed is None and parts[pind].lower() not in known_error_value:
-                        character_variables[pind] = variable_names[pind]
-            character_headers.append(character_variables)
+
 
     conn = sqlite3.connect(':memory:')
     cur = conn.cursor()
@@ -162,8 +182,7 @@ if __name__ == '__main__':
     output_variables = []
     for hind in xrange(len(file_headers)) :
         variable_names = file_headers[hind]
-        character_variables = character_headers[hind]
-        variabls_definition = ",".join(["{0} {1}".format(variable_names[ind], "text" if ind in character_variables else "real") for ind in variable_names])
+        variabls_definition = ",".join(["{0} text".format(variable_names[ind]) for ind in variable_names])
         table_constraint = "primary key ({})".format(",".join(keys))
         create_sql = "create table dataset_{0} ({1}, {2});".format(hind, variabls_definition, table_constraint)
         print create_sql
